9adf8bdcd7e8c8787c221147f8466ce51c689c8b
caetano-dev <pcaetanop@protonmail.com>
2024-09-12 13:09:37 -0300

    fish moves up and down

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 // create a command line program that will simulate a fishpot. The fish will be represented by the character `~` and the fishpot will be represented by the character `#`. The fish will move from left to right and when it reaches the end of the fishpot, it will start again from the beginning. The fishpot will have a width of 10 characters and a height of 10 characters. The fish will move every 500 milliseconds. The fish will move 10 times before the program ends.
+
 package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -17,16 +19,17 @@ func flushTerminal() {
 	fmt.Print("\033[H\033[2J")
 }
 
-func drawFishpot(horizontalFishPos int, direction string) {
+func drawFishpot(horizontalFishPos, verticalFishPos int, direction string) {
 	fmt.Println("===========================")
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			if i == height/2 && j == horizontalFishPos {
+			if i == verticalFishPos && j == horizontalFishPos {
 				if direction == "right" {
 					fmt.Print(rightFish)
 				} else {
 					fmt.Print(leftFish)
 				}
+				j += len(rightFish) - 1 // Skip the length of the fish
 			} else {
 				fmt.Print(" ")
 			}
@@ -37,20 +40,30 @@ func drawFishpot(horizontalFishPos int, direction string) {
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	horizontalFishPos := 0
+	verticalFishPos := height / 2
 	direction := "right"
 
 	for {
 		flushTerminal()
-		drawFishpot(
-
-			horizontalFishPos, direction)
+		drawFishpot(horizontalFishPos, verticalFishPos, direction)
 		time.Sleep(500 * time.Millisecond)
 
+		// Randomly move up or down
+		if rand.Intn(2) == 0 {
+			if rand.Intn(2) == 0 && verticalFishPos > 0 {
+				verticalFishPos--
+			} else if verticalFishPos < height-1 {
+				verticalFishPos++
+			}
+		}
+
+		// Move horizontally
 		if direction == "right" {
 			horizontalFishPos++
-			if horizontalFishPos >= width {
-				horizontalFishPos = width - 1
+			if horizontalFishPos >= width-len(rightFish) {
+				horizontalFishPos = width - len(rightFish)
 				direction = "left"
 			}
 		} else {
