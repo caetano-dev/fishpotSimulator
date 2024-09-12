75c228b13ee195216a8d274aac8a58daf6789bf4
caetano-dev <pcaetanop@protonmail.com>
2024-09-12 14:31:42 -0300

    Fish moves freely

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module animation
 
 go 1.22.2
+
+require golang.org/x/term v0.24.0
+
+require golang.org/x/sys v0.25.0 // indirect
--- main.go
+++ main.go
@@ -1,26 +1,32 @@
-// create a command line program that will simulate a fishpot. The fish will be represented by the character `~` and the fishpot will be represented by the character `#`. The fish will move from left to right and when it reaches the end of the fishpot, it will start again from the beginning. The fishpot will have a width of 10 characters and a height of 10 characters. The fish will move every 500 milliseconds. The fish will move 10 times before the program ends.
+// create a command line program that will simulate a fishpot. The fish can move randonly up or down and left or right.
 
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
+
+	"golang.org/x/term"
 )
 
 const (
-	width     = 20
-	height    = 10
 	rightFish = "><(((('>"
 	leftFish  = "<'(((><"
+	bubble    = "0"
 )
 
 func flushTerminal() {
 	fmt.Print("\033[H\033[2J")
 }
 
-func drawFishpot(horizontalFishPos, verticalFishPos int, direction string) {
-	fmt.Println("===========================")
+func switchDirection(direction string) string {
+	return map[string]string{"right": "left", "left": "right"}[direction]
+
+}
+
+func drawFishpot(horizontalFishPos, verticalFishPos, width, height int, direction string) {
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
 			if i == verticalFishPos && j == horizontalFishPos {
@@ -36,19 +42,28 @@ func drawFishpot(horizontalFishPos, verticalFishPos int, direction string) {
 		}
 		fmt.Println()
 	}
-	fmt.Println("===========================")
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	rand.NewSource(time.Now().UnixNano())
+	width, height, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		fmt.Println("Error getting terminal size:", err)
+		return
+	}
+
+	// Adjust width and height to leave some space for borders
+	width -= 2
+	height -= 2
+
 	horizontalFishPos := 0
 	verticalFishPos := height / 2
 	direction := "right"
 
 	for {
 		flushTerminal()
-		drawFishpot(horizontalFishPos, verticalFishPos, direction)
-		time.Sleep(500 * time.Millisecond)
+		drawFishpot(horizontalFishPos, verticalFishPos, width, height, direction)
+		time.Sleep(200 * time.Millisecond)
 
 		// Randomly move up or down
 		if rand.Intn(2) == 0 {
@@ -59,19 +74,22 @@ func main() {
 			}
 		}
 
-		// Move horizontally
+		// Randomly move left or right
+		switchDirection(direction)
 		if direction == "right" {
-			horizontalFishPos++
-			if horizontalFishPos >= width-len(rightFish) {
-				horizontalFishPos = width - len(rightFish)
+			if horizontalFishPos < width-len(rightFish) {
+				horizontalFishPos++
+			} else {
 				direction = "left"
 			}
 		} else {
-			horizontalFishPos--
-			if horizontalFishPos < 0 {
-				horizontalFishPos = 0
+			if horizontalFishPos > 0 {
+				horizontalFishPos--
+			} else {
 				direction = "right"
 			}
 		}
+
 	}
+
 }
